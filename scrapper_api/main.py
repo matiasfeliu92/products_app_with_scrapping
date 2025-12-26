405998a42ec828787ea8604c1dc96c02efb22cb2
matiasfeliu92 <matumazparrote@gmail.com>
2025-12-26 14:41:26 -0300

    update links for make tests

--- scrapper_api/main.py
+++ scrapper_api/main.py
@@ -2,9 +2,9 @@ from src.core.scrapper import Scrapper
 
 if __name__ == "__main__":
     links = [
-        "https://www.mexx.com.ar/productos-rubro/procesadores/36217-procesador-amd-ryzen-5-3400g-4.2-ghz-vega11-am4.html",
-        "https://fullh4rd.com.ar/prod/18168/placa-de-video-geforce-rtx-3060-12gb-msi-ventus-2x-oc",
-        "https://www.datasoft.com.ar/tienda/computacion/almacenamiento/discos-de-estado-solido/disco-ssd-sata-240gb-hiksemi-wave-blister"
+        "https://www.mexx.com.ar/productos-rubro/monitores/48994-monitor-gamer-24-x-micro-full-hd-ips-180hz-1ms-hdmi-x24f-kq1e.html",
+        "https://fullh4rd.com.ar/prod/17972/auricular-gamer-razer-blackshark-v2-x-wired-rz04-03240100-r3u1",
+        "https://www.datasoft.com.ar/tienda/computacion/componentes/memorias-ram/memoria-ddr4-32gb-3200-mushkin"
     ]
     for link in links:
         scrapper = Scrapper(link)

9da3c36d10401bc0113c43c08f8e2273621876db
matiasfeliu92 <matumazparrote@gmail.com>
2025-12-26 14:41:09 -0300

    add to find elements with warranty

--- scrapper_api/src/core/scrapper.py
+++ scrapper_api/src/core/scrapper.py
@@ -40,6 +40,7 @@ class Scrapper:
             discount_applicated = 100 - ((float(cash_price.text.replace("$", "").replace(".", ""))/float(list_price.text.replace("$", "").replace(".", ""))) * 100)
             installments = self.extract_elements.safe_find_elements(By.CSS_SELECTOR, self.settings.MEXX_SELECTORS["INSTALLMENTS"])
             stock = self.extract_elements.safe_find_elements(By.CSS_SELECTOR, self.settings.MEXX_SELECTORS["STOCK"])
+            warranty = self.extract_elements.safe_find_elements(By.CSS_SELECTOR, self.settings.MEXX_SELECTORS["WARRANTY"])
 
             self.product_data["name"] = product_title.text if product_title else ""
             self.product_data["sku"] = sku.text.replace("CÓDIGO:", "") if sku else ""
@@ -51,6 +52,7 @@ class Scrapper:
             self.product_data["discount_applicated"] = round(discount_applicated, 2)
             self.product_data["installments"] = installments.text if installments and type(installments) != list else ""
             self.product_data["stock"] = "Available" if "EN STOCK" in stock.text else "Not Available"
+            self.product_data["warranty"] = warranty.text if warranty else ""
             self.product_data["store"] = "Mexx"
             logging.info(f"------------PRODUCT DATA------------>{self.product_data}")
             logging.info(
@@ -90,6 +92,7 @@ class Scrapper:
             web_stock = self.extract_elements.safe_find_elements(By.CSS_SELECTOR, self.settings.FULLH4RD_SELECTORS["WEB_STOCK"])
             local_stock = self.extract_elements.safe_find_elements(By.CSS_SELECTOR, self.settings.FULLH4RD_SELECTORS["LOCAL_STOCK"])
             logging.info(f"WEB STOCK ----> {web_stock.text}, LOCAL STOCK ----> {local_stock.text}")
+            warranty = self.extract_elements.safe_find_elements(By.XPATH, self.settings.FULLH4RD_SELECTORS["WARRANTY"])
 
             self.product_data["name"] = product_title.text if product_title else ""
             self.product_data["sku"] = sku.text if sku else ""
@@ -101,6 +104,7 @@ class Scrapper:
             self.product_data["discount_applicated"] = discount_applicated.text if discount_applicated else ""
             self.product_data["installments"] = installments_options
             self.product_data["stock"] = "Available" if "SIN STOCK" not in web_stock.text or "SIN STOCK" not in local_stock.text else "Not Available"
+            self.product_data["warranty"] = warranty.text if warranty else ""
             self.product_data["store"] = "Fullh4rd"
             logging.info(f"------------PRODUCT DATA------------>{self.product_data}")
             logging.info(
@@ -129,6 +133,7 @@ class Scrapper:
             installments_options = {}
             for i, inst in enumerate(n_instalments):
                 installments_options[f"option {i+1}"] = f"{inst} cuotas"
+            warranty = self.extract_elements.safe_find_elements(By.XPATH, self.settings.DATASOFT_SELECTORS["WARRANTY"])
 
             self.product_data["name"] = product_title.text if product_title else ""
             self.product_data["sku"] = sku.text if brand else ""
@@ -140,6 +145,7 @@ class Scrapper:
             self.product_data["discount_applicated"] = round(discount_applicated, 2)
             self.product_data["installments"] = installments_options
             self.product_data["stock"] = "Available"
+            self.product_data["warranty"] = warranty.text if warranty else ""
             self.product_data["store"] = "Datasoft"
             logging.info(f"------------PRODUCT DATA------------>{self.product_data}")
             logging.info(

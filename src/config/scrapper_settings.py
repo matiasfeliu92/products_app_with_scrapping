7586ca7c443c0c51d0909f48ad02b780d6df1533
matiasfeliu92 <matumazparrote@gmail.com>
2026-01-06 13:01:31 -0300

    update class name Settings for ScrapperSettings, to include only scrapping config

---
+++ src/config/scrapper_settings.py
@@ -0,0 +1,99 @@
+import logging
+import platform
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+class ScrapperSettings:
+    def __init__(self):
+        self.USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+        self.STORES = [
+            "mexx", 
+            "fullh4rd", 
+            "datasoft",
+            "armytech",
+            "compragamer"
+        ]
+        self.MEXX_SELECTORS = {
+            "PRODUCT_TITLE": "h1",
+            "SKU": "#prod_desc_edit > div.row.pr-0.pl-0.filaMarcas > div > h6:nth-child(2)",
+            "BRAND": "body > div.section.single.ecommerce-page.mt-0.pt-0 > div > div > div:nth-child(9) > div:nth-child(3) > span:nth-child(1) > p",
+            "CATEGORY_PATH": "div.col-md-12.pt-2.pb-2 > ul.breadcrum.nav.navbar-nav.navbar-left",
+            "LIST_PRICE": "#precio > span > b.mr-2.ft-3.PRECIO_NEW",
+            "CASH_PRICE": "#prod_desc_edit > h2 > b:nth-child(2)",
+            "INSTALLMENTS": "#precio > span > b.anterior_single.preciobot2",
+            "STOCK": "#prod_desc_edit > div:nth-child(3) > h6 > div",
+            "WARRANTY": "div.garantia-bloque p"
+        }
+        self.FULLH4RD_SELECTORS = {
+            "PRODUCT_TITLE": "h1",
+            "SKU": "p.codebar",
+            "BRAND": "",
+            "CATEGORY_PATH": 'a > span[itemprop="item"] > span[itemprop="name"]',
+            "INSTALLMENTS": "div.price-special-container > p",
+            "LIST_PRICE": "",
+            "CASH_PRICE": "div.price-list-container > p > span.bold",
+            "DISCOUNT_APPLICATED": "div.price-list-container > p:nth-child(1) > span:nth-child(2)",
+            "WEB_STOCK": "div.stock-container > div:nth-child(1) > h5",
+            "LOCAL_STOCK": "div.stock-container > div:nth-child(2) > h5",
+            "WARRANTY": "//li[contains(text(), 'Garantía')]"
+        }
+        self.DATASOFT_SELECTORS = {
+            "PRODUCT_TITLE": "h1 > span",
+            "SKU": 'span[itemprop="sku"]',
+            "BRAND": 'div.short_desc > ul > li:nth-child(1)',
+            "CATEGORY_PATH": 'div.col-xs-12 > ul.breadcrumb > li > span > a[itemprop="url"] > span[itemprop="title"]',
+            "INSTALLMENTS": "div.block.pago > div:nth-child(1) > span",
+            "LIST_PRICE": "span.pesos > strong",
+            "CASH_PRICE": "#final_price",
+            "DISCOUNT_APPLICATED": "",
+            "WARRANTY": "//td[contains(text(), 'Garantía')]/following-sibling::td"
+        }
+        self.ARMYTECH_SELECTORS = {
+            "PRODUCT_TITLE": "/html/body/main/section/div[2]/div/section/div[2]/div[1]/div[2]/div/div[1]/h1/span",
+            "SKU": "/html/body/main/section/div[2]/div/section/div[2]/div[1]/div[2]/div/div[1]/div[1]/span",
+            "BRAND": "/html/body/main/section/div[2]/div/section/div[2]/div[2]/section/div/div/div[1]/a",
+            "CATEGORY_PATH": '',
+            "INSTALLMENTS": "/html/body/main/section/div[2]/div/section/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div[1]/p[1]",
+            "LIST_PRICE": "/html/body/main/section/div[2]/div/section/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div[2]",
+            "CASH_PRICE": "/html/body/main/section/div[2]/div/section/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[1]/div/span/span[3]",
+            "DISCOUNT_APPLICATED": "",
+            "WARRANTY": ""
+        }
+        self.COMPRA_GAMER_SELECTORS = {
+            "PRODUCT_TITLE": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/div[2]/h1/span",
+            "SKU": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/div[3]/mat-chip-listbox/div/mat-chip-option[1]/span[2]/button/span[2]/span[1]",
+            "BRAND": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/div[1]/span[2]/span",
+            "CATEGORY_PATH": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/div[1]/span[1]",
+            "BUTTON_FOR_INSTALLMENTS": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/cgw-product-prices/div/div[2]/div/div[1]/span",
+            "INSTALLMENTS": "/html/body/div[3]/div[2]/div/mat-dialog-container/div/div/cgw-installments-modal-selector-multiple/div/div/table/tbody/tr[{}]/td[{}]/span",
+            "LIST_PRICE": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/cgw-product-prices/div/div[2]/div/div[1]/div/cgw-price/span/span[2]",
+            "CASH_PRICE": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/cgw-product-prices/div/div[1]/div/cgw-price/span/span[2]",
+            "STOCK": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/div[4]/div[1]/span",
+            "WARRANTY": "/html/body/cgw-root/cgw-core/div/div[2]/div/main/cgw-product/div/cgw-base-card[1]/div/div/div/div[2]/div[4]/div[2]/div/span"
+        }
+
+    def get_chrome_driver(self):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument(f"--user-agent={self.USER_AGENT}")
+
+        if platform.system() == "Windows":
+            service = Service(ChromeDriverManager().install())
+            return webdriver.Chrome(service=service, options=options)
+
+        options.binary_location = "/usr/bin/chromium"
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        service = Service("/usr/bin/chromedriver")
+        return webdriver.Chrome(service=service, options=options)
